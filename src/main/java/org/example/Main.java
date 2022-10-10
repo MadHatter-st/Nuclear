4f08f47ab4b78ebd8396651079201b4bd55fbe82
Rainbow49 <n_khmelev@mail.ru>
2022-10-10 17:44:14 +0900

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Main {
+        public static void main(String[] args) {
+            Nuclear nuc =  new Nuclear();
+
+            Thread threadInc = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(!nuc.stop){
+
+                        if(nuc.pos||nuc.level>nuc.temp){
+                            System.out.println("Thread Increser| temp: "+ nuc.change(true));
+                        }else{
+                            nuc.pos = nuc.level == nuc.temp?true:false;
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }if(nuc.getTemp()>=100){
+                        nuc.stop = true;
+                    }
+                }
+            });
+
+            Thread threadDec = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                        while(!nuc.stop){
+                            if(nuc.pos||nuc.level<nuc.temp){
+                                System.out.println("Thread Decreser| temp: "+nuc.change(false));
+                            }else{
+                                nuc.pos = nuc.level == nuc.temp?true:false;
+                            }
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    if(nuc.getTemp()<=0){
+                        nuc.stop = true;
+                    }
+                }
+            });
+
+            Thread threadOp = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(!nuc.stop){
+                        nuc.operator();
+                        System.out.println("Thread Operator| temp: "+nuc.level);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            });
+
+            threadDec.start();
+            threadInc.start();
+            threadOp.start();
+        }
+    }
